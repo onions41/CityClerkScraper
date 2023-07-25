d23c88ff892d62df6eff956b3c71929fd5e59ccc
onions41 <kblee89@outlook.com>
2023-07-24 21:15:52 -0700

    Added logger, service

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-using Scraper;
+using Scraper.Richmond;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+IConfiguration config = builder.Configuration;
 
+builder.Logging.AddFile(config.GetSection("Logging"));
+
+builder.Services.AddSingleton<IRichmond, Richmond>();
+
+builder.Services.AddHostedService<ScrapingService>();
+
+// builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+// builder.Services.AddSingleton<IMeetingData, MeetingData>();
+// builder.Services.AddSingleton<IRecordData, RecordData>();
+// builder.Services.AddSingleton(new OpenAIClient(config.GetSection("ApiKeys")["OpenAi"]));
+// builder.Services.AddSingleton<IDivideDocumentPrompt, DivideDocumentPrompt>();
+
+IHost host = builder.Build();
 host.Run();
