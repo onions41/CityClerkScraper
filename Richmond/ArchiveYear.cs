07915d88b21517031e41465b9f71656cb1b7e19c
onions41 <kblee89@outlook.com>
2023-08-08 12:14:27 -0700

    Element that returns Richmond Meetings

--- Richmond/ArchiveYear.cs
+++ Richmond/ArchiveYear.cs
@@ -1,8 +1,4 @@
-using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-
-using DataAccess.Data;
-
 using HtmlAgilityPack;
 
 namespace Scraper.Richmond;
@@ -14,98 +10,93 @@ namespace Scraper.Richmond;
 // This page is a portal containing links to resources, like meeting minutes, meeting recorded videos, etc.
 // Richmond website archives are organized like this. 
 // Archives -> Meeting type -> Year -> Resources 
-internal class ArchiveYear {
-  private readonly HtmlDocument _page;
-  private readonly string _meetingType;
+internal class ArchiveYear
+{
+	private readonly string _meetingType;
+	private readonly HtmlDocument _page;
 
-  public ArchiveYear(Uri archiveYearUri, string meetingType) {
-    // HtmlAgilityPack parser
-    _page = new HtmlWeb().Load(archiveYearUri)
-      ?? throw new ArgumentException("_page", $"Could not parse the page at {archiveYearUri.ToString()}");
+	public ArchiveYear(Uri uri, string meetingType) {
+		_meetingType = meetingType;
+		_page = new HtmlWeb().Load(uri) ??
+		        throw new ArgumentException("_page", $"Could not parse the page at {uri.ToString()}");
+		Uri = uri;
+	}
 
-    _meetingType = meetingType;
-  }
+	public Uri Uri { get; private init; }
 
-  public IEnumerable<MeetingElement> GetMeetingElements() {
-    // <tr> elements that contain each meeting's information and resource links
-    HtmlNodeCollection meetingRows = _page.DocumentNode.SelectNodes(
-      "//table[@id='ctl00_ctl00_main_main_GridViewPrevious']/tr"
-    ) ?? throw new ArgumentNullException("meetingRows", "Could not find the <tr> elements containing each meeting's information");
+	public IEnumerable<Meeting> GetMeetings(DateTime startDate, DateTime endDate) {
+		// <tr> elements that contain each meeting's information and resource links
+		var meetingRows = _page.DocumentNode.SelectNodes(
+			"//table[@id='ctl00_ctl00_main_main_GridViewPrevious']/tr"
+		) ?? throw new Exception("Could not find the <tr> elements containing each meeting's information");
 
-    foreach (var meetingRow in meetingRows) {
-      // The first <span> contains the date of the meeting
-      HtmlNode node = meetingRow.SelectSingleNode(".//span");
-      if (node is not null) {
-        DateTime date;
-        // Tries to parse the date of the meeting
-        bool success = DateTime.TryParseExact(
-          node.InnerHtml,
-          "MMMM d, yyyy",
-          CultureInfo.InvariantCulture,
-          DateTimeStyles.None,
-          out date
-        );
-        // Could not parse the date
-        if (success is false) {
-          throw new Exception("Could not parse the date of the meeting");
-        }
+		foreach (var meetingRow in meetingRows) {
+			// The first <span> contains the date of the meeting
+			var node = meetingRow.SelectSingleNode(".//span");
 
-        for (int i = 1; i <= 2; i++) {
-          // ChildNode[1] is the <td> that contains the links to the meeting records of the current type
-          // ChildNode[2] is the <td> that contains the links to the "in camera" meeting which occured on the same day
-          HtmlNodeCollection resourceLinks = meetingRow.ChildNodes[1].SelectNodes(".//a");
+			if (node is null) continue;
 
-          // Yields the MeetingElement if there were links to resources connected to the meeting was/were found.
-          // If resourceLinks is null, it means there was no meeting
-          if (resourceLinks is not null) {
-            // Loads link href values into a list
-            List<Uri> resourceUris = new();
-            foreach (var link in resourceLinks) {
-              try {
-                resourceUris.Add(new Uri(link.Attributes["href"].Value));
-              } catch (Exception) {
-                // Error if Uri cannot be created because the href value is invalid, just skip
-              }
-            }
+			// Tries to parse the date of the meeting
+			var success = DateTime.TryParseExact(
+				node.InnerHtml,
+				"MMMM d, yyyy",
+				CultureInfo.InvariantCulture,
+				DateTimeStyles.None,
+				out var date
+			);
+			// Could not parse the date
+			if (success is false) {
+				throw new Exception("Could not parse the date of the meeting");
+			}
 
-            if (i == 1) {
-              yield return new MeetingElement(date, _meetingType, resourceUris);
-            } else if (i == 2) {
-              yield return new MeetingElement(date, "in camera", resourceUris);
-            }
-          }
-        }
-      }
-    }
-  }
+			// If the date of the meeting falls outside of the desired date range
+			if (date < startDate || endDate < date) continue;
+
+			for (var i = 1; i <= 2; i++) {
+				// ChildNode[1] is the <td> that contains the links to the meeting records of the current type
+				// ChildNode[2] is the <td> that contains the links to the "in camera" meeting which occured on the same day
+				var resourceLinks = meetingRow.ChildNodes[i].SelectNodes(".//a");
+
+				// Don't return a meeting if there are no <a> tags associated with it
+				if (resourceLinks is null) {
+					continue;
+				}
+
+				// Loads link href values into a list
+				List<Uri> resourceUris = new();
+				foreach (var link in resourceLinks) {
+					try {
+						// Deal with relative URLs
+						var url = link.Attributes["href"].Value;
+						Uri resourceUri;
+						if (!string.IsNullOrEmpty(url) && url[0] == '/') {
+							resourceUri = new Uri(RichmondWebsite.BaseUri, url);
+						} else {
+							resourceUri = new Uri(url);
+						}
+
+						resourceUris.Add(resourceUri);
+					}
+					catch (Exception) {
+						// Error if Uri cannot be created because the href value is invalid, just skip
+					}
+				}
+
+				// Even if <a> tags were found, it's possible that all of them were invalid
+				// Don't return a meeting if so.
+				if (resourceUris.Count == 0) {
+					continue;
+				}
+
+				switch (i) {
+					case 1:
+						yield return new Meeting(date, _meetingType, resourceUris);
+						break;
+					case 2:
+						yield return new Meeting(date, "in camera meeting", resourceUris);
+						break;
+				}
+			}
+		}
+	}
 }
-
-  // Yields URLs for all resources found on the page
-  // TODO: Should take dates in parameters and yield only the resources that fit the range
-  // public IEnumerable<Uri> GetResourceUris()
-  // {
-  //   // HtmlAgilityPack parser
-  //   HtmlDocument yearPage = new HtmlWeb().Load(_archiveYearUri);
-
-  //   HtmlNodeCollection resourceLinks = yearPage.DocumentNode.SelectNodes(
-  //     "//table[@id='ctl00_ctl00_main_main_GridViewPrevious']//a"
-  //   ) ?? throw new ArgumentNullException("No links could be retrieved from this year's archive page");
-
-  //   foreach (var link in resourceLinks)
-  //   {
-  //     Uri uri;
-  //     try
-  //     {
-  //       uri = new(link.Attributes["href"].Value);
-  //     }
-  //     catch (Exception)
-  //     {
-  //       // Just skip broken links
-  //       continue;
-  //     }
-  //     yield return uri;
-  //   }
-  // }
-
-
-
