c39d4b1dccce1f594c76bd276235f4e01a91ad46
onions41 <kblee89@outlook.com>
2023-08-14 16:58:55 -0700

    Wrote Save()

--- Common/MeetingBase.cs
+++ Common/MeetingBase.cs
@@ -1,10 +1,18 @@
 using DataAccess.Models;
+using DataAccess.Data;
 
 namespace Scraper.Common;
 
 internal abstract class MeetingBase
 {
-	public MeetingModel Model { get; protected init; }
+	public MeetingModel? Model { get; protected init; }
+	
+	public async Task Save(IMeetingData meetingData) {
+   	if (Model is null) throw new Exception("Cannot save meeting before its Model is initialized");
+   	var meetingId = await meetingData.InsertMeeting(Model);
+   	Model.Id = meetingId;
+   }
+	
 	public abstract IEnumerable<DocumentBase> GetDocuments();
 	public abstract IEnumerable<Video> GetVideos();
 }
