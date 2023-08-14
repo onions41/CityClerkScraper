96e884e03d8651964544e2c0172090b38e9e2a45
onions41 <kblee89@outlook.com>
2023-08-14 16:59:06 -0700

    Wrote Save()

--- Common/Video.cs
+++ Common/Video.cs
@@ -1,16 +1,21 @@
+using DataAccess.Data;
 using DataAccess.Models;
 
 namespace Scraper.Common;
 
 internal class Video
 {
-	public Video(Uri uri, string type) {
-		Model = new VideoModel() {
-			Url = uri.ToString()
-		};
-		MeetingsVideosType = type;
-	}
-	
-	public VideoModel Model { get; }
-	public string MeetingsVideosType { get;}
+   public Video(Uri uri, string type) {
+      Model = new VideoModel() {
+         Url = uri.ToString()
+      };
+      MeetingsVideosType = type;
+   }
+
+   public VideoModel Model { get; }
+   public string MeetingsVideosType { get; }
+
+   public async Task Save(IVideoData videoData) {
+      Model.Id = await videoData.InsertVideo(Model);
+   }
 }
